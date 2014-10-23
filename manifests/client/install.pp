42df72bbd68257f8e7fbfd0042d02e0dd67aecad
ondmagi <andreas@superblock.se>
2014-10-23 13:02:42 +0200

    client install working

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -1,40 +1,26 @@
 class netbackup::client::install (
-  $installer_path       = '/mnt/install/netbackup/clients/install',
-  $netbackup_version    = '7.6.0.2',
+  $installer_path       = undef,
+  $netbackup_version    = undef,
   $netbackup_master     = "netbackup.${::domain}",
   $netbackup_clientname = "${::fqdn}",
 ){
 
-  $installer_os = $::kernel ? {
-    'Linux' => 'Linux',
-    default => 'Linux',
-  }
-
-  $installer_dist = $::osfamily ? {
-    'Debian' => 'Debian',
-    'RedHat' => 'RedHat',
-    default  => undef,
-  }
-
-  # @todo FIXME
-  $installer_minversion = $::osfamily ? {
-    'Debian' => '2.6.18',
-    'RedHat' => '2.6.18',
-    'SuSE'   => '2.6.16',
-  }
-
-  file { '/tmp/install_netbackup_client.expect':
+  file { 'install_netbackup_client.expect':
+    path     => '/tmp/install_netbackup_client.expect',
     owner    => 'root',
     group    => 'root',
     mode     => '0744',
     content  => template('netbackup/install_netbackup_client.expect.erb'),
   }
 
+  package { 'expect':
+    ensure => installed,
+  }
   exec { 'run-netbackup-install':
     command  => 'expect /tmp/install_netbackup_client.expect',
     path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    requires => Package['expect'],
-    onlyif   => "grep -v ${netbackup_version} /usr/openv/netbackup/bin/version",
+    require  => [Package['expect'], File['install_netbackup_client.expect']],
+    unless   => "grep ${netbackup_version} /usr/openv/netbackup/bin/version",
   }
 
 }
